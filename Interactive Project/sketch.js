a218ff918b4b58b08b80eefb7b85776fc2c3c636
IWillObliterateYou <161519475+IWillObliterateYou@users.noreply.github.com>
2024-03-06 14:11:57 -0600

    Updated I.P.

--- Interactive Project/sketch.js
+++ Interactive Project/sketch.js
@@ -4,36 +4,62 @@ let rectWidth;
 let rectHeight;
 let buttonX;
 let buttonY;
+let buttonsToEnter;
+let i;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  fill("red");
-  
+
   initialStateStart = true; // is the game at its default state
-  rectWidth = random(35, 101); // size range of button width-wise
-  rectHeight = random(25, 71); // size range of button heigthwise
-  buttonX = random(0, width - rectWidth); // x pos of initial button
-  buttonY = random(0, height - rectHeight); // y pos of initial button
+  rectWidth = width / 7; // size range of button width-wise
+  rectHeight = height / 7; // size range of button heigthwise
+  buttonX = width / 2 - rectWidth / 2; // x pos of initial button
+  buttonY = height / 3 * 2 - rectHeight / 4; // y pos of initial button
+  buttonsToEnter = random(1, 7); // number of times you must hit the button after the initial press for the game to load
 }
 
 function draw() {
   background("black");
   startScreen();
+
 }
 
 // create and manage the starting screen
 function startScreen() {
-  if (initialStateStart) {
-    rect(buttonX, buttonY, rectWidth, rectHeight, 5);
+  fill("white");
+  textSize(30);
+  textAlign(CENTER, CENTER);
+  text("Welcome", width / 2, height / 2);
 
-    // is the button pressed
-    if (mouseIsPressed && mouseX >= buttonX && mouseX <= buttonX + rectWidth && mouseY >= buttonY && mouseY <= buttonY + rectHeight) {
-      //initialStateStart = !initialStateStart; // trigger identifiable change to tell the button it can move now
-      fill("blue");
-    }
+  drawButton();
+
+  // is the button pressed
+  if (initialStateStart) {
+// add to if statement \/ checking if button pressed less than number of times
+
+
+//    for (i = 0; i < buttonsToEnter; i ++) {
+      if (mouseIsPressed && mouseX >= buttonX && mouseX <= buttonX + rectWidth && mouseY >= buttonY && mouseY <= buttonY + rectHeight) {
+        initialStateStart = !initialStateStart; // trigger identifiable change to tell the program the button has been pressed for the first time
+        moveButton();
+        drawButton();
+      }
+    console.log("done");
   }
 }
 
-function moveButton() {
-  
+function moveButton() { // moves the button to a new location 
+  buttonX = random(0, width - rectWidth);
+  buttonY = random(0, height - rectHeight);
+  rectWidth = random(65, 121);
+  rectHeight = random(25, 91);
+  initialStateStart = !initialStateStart;
+}
+
+function drawButton() {
+  fill("red");
+  rect(buttonX, buttonY, rectWidth, rectHeight, 5);
+  fill("black");
+  textAlign(CENTER, CENTER);
+  text("Press", buttonX + rectWidth / 2, buttonY + rectHeight / 2);
 }
