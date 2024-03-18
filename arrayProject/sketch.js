bfa909bd00a2dc341741bb6015a470b52924cdaf
IWillObliterateYou <161519475+IWillObliterateYou@users.noreply.github.com>
2024-03-18 14:11:25 -0600

    Started on array project

--- arrayProject/sketch.js
+++ arrayProject/sketch.js
@@ -6,10 +6,35 @@
 // - describe what you did to take this project "above and beyond"
 
 
+// Stardew style terrain generators
+
+let terrainLevelOne = [];
+let numberOfLayers = 20;
+let tileSize;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  generateLayer;
 }
 
 function draw() {
-  background(220);
+  background("green");
+  for (let thisTile of terrainLevelOne) {
+    square()
+  }
+}
+
+function createTile(cornerX, tileSize) {
+  let thisTile = {
+    x: cornerX,
+    y: 0,
+    s: tileSize,
+  }
+  terrainLevelOne.push;
+}
+
+function generateLayer() {
+  for (let i; i < width / tileSize; i ++) {
+    createTile(i * tileSize, tileSize);
+  }
 }
