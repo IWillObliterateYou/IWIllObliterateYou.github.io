3b4b59a22d3020dcd9cde017b8e4d51b4bbe0be0
IWillObliterateYou <161519475+IWillObliterateYou@users.noreply.github.com>
2024-04-15 13:12:13 -0600

    tiny edit to 2d project

--- 2dArrayProject/sketch.js
+++ 2dArrayProject/sketch.js
@@ -5,6 +5,14 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// in the level arrays
+// 0 is an empty space
+// 1 is a wall
+// 2 is a basic enemy
+// 
+
+let tileSize = 20;
+
 let levelOne = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -26,8 +34,8 @@ function draw() {
 }
 
 function drawLevel(level) {
-  s
-  for (let tileX of level) {
-    if ()
-  }
+  let xTilePosition = Math.floor(mouseX / tileSize);
+  let yTilePosition = Math.floor(mouseY / tileSize);
+
+
 }
